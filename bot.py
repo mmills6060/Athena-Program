47e756232cf4516b1d63d54e24bc6082f80b9f16
Michael Mills <mmills6060@gmail.com>
2022-02-09 22:59:27 -0500

    updated to V.20

---
+++ bot.py
@@ -0,0 +1,13 @@
+
+import ccxt
+
+print(ccxt.exchanges)
+
+#exchange_id = 'binance'
+#exchange_class = getattr(ccxt, exchange_id)
+#exchange = exchange_class({
+ #   "apiKey": 'YOUR_API_KEY' ,
+  #  'secret': 'YOUR_SECRET' ,
+   # 'timeout': 30000',
+    #'enableRateLimit': True
+#    }
